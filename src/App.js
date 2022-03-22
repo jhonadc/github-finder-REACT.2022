abca0dc4216e69157a12d0d5502007e902172811
jhonadc <jhonathanaugusto@gmail.com>
2022-03-22 16:08:07 +0100

    footer ready - dinamic year and svg image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Route } from 
-'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div className='flex flex-col justify-between 
+      <div
+        className='flex flex-col justify-between 
       h-screen'>
         <Navbar />
 
         <main>Content</main>
+        <Footer />
       </div>
     </Router>
   );
