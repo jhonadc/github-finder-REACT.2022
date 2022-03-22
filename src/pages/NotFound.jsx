c86be65fe4f98947e6a4383555524270684b8714
jhonadc <jhonathanaugusto@gmail.com>
2022-03-22 16:42:04 +0100

    NotFound page content added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,24 @@
-function NotFound() {
-  return (
-    <div>
-      Not Found
-    </div>
-  )
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+{
+  /*to have a link back Home */
 }
 
-export default NotFound
+function NotFound() {
+  return (
+    <div className='hero'>
+      <div className='text-center hero-content'>
+        <div className='max-w-lg'>
+          <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
+          <p className='text-5xl mb-8'> 404 - Page not found!</p>
+          <Link to='/' className='btn btn-primary brn-lg'>
+            <FaHome className='mr-2' />
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
