ee1a14f46ffdbc48e0fa4bf2953e0149913d5b1a
jhonadc <jhonathanaugusto@gmail.com>
2022-03-22 12:13:42 +0100

    Navbar completed with icon, and Home.About btns

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,7 +7,29 @@ function Navbar({ title }) {
     <nav
       className='navbar mb-12 shadow-lg bg-neutral 
   text-neutral-content'>
-      Navbar
+      {/* full page */}
+      <div className='container mx-auto'>
+        {/* margin auto l and r */}
+        <div className='flex-none px-2 mx-2'>
+          {/* padding and margin on x axis */}
+          <FaGithub className='inline pr-2 text-3xl' />
+          {/* inline text next to it. padding right. change size*/}
+          <Link to='/' className='text-lg font-bold align-middle'>
+            {title}
+          </Link>
+        </div>
+
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost brn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost brn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
